76ca30ea6377b153196a29d0fb6ae4a5360f815d
Silver Hage <silverhage@Silvers-MacBook-Pro-2.local>
2014-11-14 16:57:05 +0200

    Bug fixes

--- listworks/js/grammar.js
+++ listworks/js/grammar.js
@@ -1,6 +1,17 @@
-// List of grammar points from Google Docs via API
-
 $(document).ready(function() {	
+	
+
+
+	// Toggles button focus and scrolls view to active button
+    $(document).on("click", ".btn", function(e) {
+	    $(this).toggleClass("focus");
+        
+        $('html, body').animate({
+		    scrollTop: $(this, ".focus").offset().top -72
+		}, 300, 'swing');
+	});
+
+    // List of grammar points from Google Docs via API
 	$(function listGrammar() {	
 	$.getJSON( "https://spreadsheets.google.com/feeds/list/1DXI_J2wn7C3mDRGSWwh5lJZ0rdAArYfX30jPGm-A_Fo/od6/public/values?alt=json-in-script&callback=?",
 		function (data) {	
@@ -12,15 +23,10 @@ $(document).ready(function() {
 				var example2 = entry.gsx$example2.$t;
 				var example3 = entry.gsx$example3.$t;
 				var notes = entry.gsx$notes.$t;
+				console.log(notes.length);
 
-				$('div.row').append('<div class="col-xs-12"><button type="button" class="btn" data-toggle="collapse" data-parent="#accordion" data-target="#' + grammar + '"><div><span class="grammar">' + grammar + '</span><span class="buttonsample">' + example1 + '</span><span class="explanation">' + explanation + '</span></div></button><div id="' + grammar + '" class="clearfix collapse collapsed">' + (notes.length != 0 ? '<small>Notes</small><div class="notes">' + notes + '</div>' : '') + '<small>Requires</small><h4>' + requires + '</h4><small>Sample sentences</small><div class="sample"><span class="glyphicon glyphicon-chevron-right"></span><mark>' + example1 + '</mark></div><div class="sample"><span class="glyphicon glyphicon-chevron-right"></span><mark>' + example2 + '</mark></div><div class="sample3"><span class="glyphicon glyphicon-chevron-right"></span><mark>' + example3 + '</mark></div></div></div>');
+				$('div.row').append('<div class="col-xs-12"><button type="button" class="btn" data-toggle="collapse" data-parent="#accordion" data-target="#' + grammar + '"><div><span class="grammar">' + grammar + '</span><span class="buttonsample">' + example1 + '</span><span class="explanation">' + explanation + '</span></div></button><div id="' + grammar + '" class="clearfix collapse collapsed"><div class="inside">' + (notes.length != 0 ? '<small>Notes</small><div class="notes">' + notes + '</div>' : '') + '<small>Requires</small><h4>' + requires + '</h4><small>Sample sentences</small><div class="sample"><span class="glyphicon glyphicon-chevron-right"></span><mark>' + example1 + '</mark></div>' + (example2.length != 0 ? '<div class="sample3"><span class="glyphicon glyphicon-chevron-right"></span><mark>' + example2 + '</mark></div>' : '') + (example3.length != 0 ? '<div class="sample3"><span class="glyphicon glyphicon-chevron-right"></span><mark>' + example3 + '</mark></div>' : '') + '</div></div></div>');
 				
-				// Hides empty examples
-				if (example3.length === 0) {
-					$(".mark", ).addClass("hidden");
-				}
-
-
 
 
 				// List.js for filtering
@@ -34,28 +40,7 @@ $(document).ready(function() {
 			});
 			});
 		});
-
-	// toggleclass this btn.focus myel ==this
-	$(".btn").click(function() {
-	  console.log('click');
-	  //$( this ).toggleClass("focus");
-
 	});
-
-	// Scroll thingy
-	$(function () {
-		$("#accordion").on("shown.bs.collapse", function () {
-		    var myEl = $(this).find('.in');
-
-		    $('html, body').animate({
-		        scrollTop: $(myEl).offset().top -124
-		    }, 300, 'swing');
-		});
-	});
-
-	});
-
-
 
 // Clear Search
 
